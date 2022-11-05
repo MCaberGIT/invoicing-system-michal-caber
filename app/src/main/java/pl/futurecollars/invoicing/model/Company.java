de0ad33ab212faf03b941659ebb78fdd08501778
Michal Caber <javacaber@gmail.com>
2022-11-06 00:49:49 +0100

    lesson2 task 1

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -8,4 +8,10 @@ public class Company {
   private String taxIdentificationNumber;
   private String address;
   private String name;
+
+  public Company(String taxIdentificationNumber, String address, String name) {
+    this.taxIdentificationNumber = taxIdentificationNumber;
+    this.address = address;
+    this.name = name;
+  }
 }
--- app/src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
@@ -10,4 +10,11 @@ public class InvoiceEntry {
   private BigDecimal price;
   private BigDecimal vatValue;
   private Vat vatRate;
+
+  public InvoiceEntry(String description, BigDecimal price, BigDecimal vatValue, Vat vatRate) {
+    this.description = description;
+    this.price = price;
+    this.vatValue = vatValue;
+    this.vatRate = vatRate;
+  }
 }
